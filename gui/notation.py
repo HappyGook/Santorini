268e47b7583a8e33549fade814610e92f708d02c
oleg77 <olegradzhabov@gmail.com>
2025-09-26 09:48:06 +0200

    Notation class + more funcs

--- gui/notation.py
+++ gui/notation.py
@@ -1,15 +1,33 @@
-from game.board import get_legal_moves, BoardState
+from typing import Optional
+from datetime import datetime
+from game.models import Worker, Coord
 
 def coords_to_notation(pos):
     row, col = pos
-    return f"{chr(97 + row)}{col+1}"   # 'a'..'e' and 1..5
+    return f"{chr(97 + col)}{row+1}"   # 'a'..'e' and 1..5
 
-def get_legal_notation(board: BoardState, player: str):
-    moves = get_legal_moves(board, player)
-    notation = []
-    for worker_pos, new_pos, build_pos in moves:
-        worker_str = coords_to_notation(worker_pos)
-        new_str = coords_to_notation(new_pos)
-        build_str = coords_to_notation(build_pos)
-        notation.append(f"{worker_str}-{new_str},{build_str}")
-    return notation
+class GameNotation:
+    def __init__(self):
+        self.moves = []
+
+    def record_setup(self, worker: Worker):
+        pos_str = coords_to_notation(worker.pos)
+        self.moves.append(f"setup {worker.owner} {worker.id} {pos_str}")
+
+    def record_turn(self, worker: Worker, move_to: Coord, build_at: Coord):
+        worker_str = coords_to_notation(worker.pos)
+        move_str = coords_to_notation(move_to)
+        build_str = coords_to_notation(build_at)
+        self.moves.append(f"{worker_str}-{move_str},{build_str}")
+
+    def get_notation(self):
+        return "\n".join(self.moves)
+
+    def save(self, filename: Optional[str] = None):
+        if filename is None:
+            filename = datetime.now().strftime("game%d.%m.%Y.san")
+        with open(filename, "w") as f:
+            f.write(self.get_notation())
+
+    def clear(self):
+        self.moves.clear()
