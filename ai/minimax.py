8472b9717d6d1328dc5fc3ac260f30813ae76bfb
oleg77 <olegradzhabov@gmail.com>
2025-10-03 19:00:26 +0200

    Minimax iteration fixes -- IT'S ALIVE!

--- ai/minimax.py
+++ ai/minimax.py
@@ -1,47 +1,46 @@
 from typing import Tuple, Optional, List
-
 from ai.heuristics import evaluate
 from game.moves import move_worker, build_block
-from game.rules import all_legal_actions, is_win_after_move
+from game.rules import legal_moves, legal_builds  # not all_legal_actions
 
 # inifinity const for evaluation win/lose
 INF = 10**9
-
 # the type for ai's action (worker, move, build)
-Action = Tuple[object, tuple[int, int], tuple[int, int]]
+Action = Tuple[object, Tuple[int,int], Tuple[int,int]]
 
-def minimax(board, depth: int, player_id: str, max_player_id: str,
-            alpha=-INF, beta=INF, maximizing=True) -> Tuple[int, Optional[Action]]:
+def minimax(board, depth: int, player_id: str, max_player_id: str, alpha=-INF, beta=INF, maximizing=True) -> Tuple[int, Optional[Action]]:
     """
-    alpha-beta minimax strategy (i hope this works)
-    :param board: current board state
-    :param depth: how deep we want to search
-    :param player_id: whose turn it is at this node
-    :param max_player_id: the player we evaluate from/for
+       alpha-beta minimax strategy (i hope this works)
+       :param board: current board state
+       :param depth: how deep we want to search
+       :param player_id: whose turn it is at this node
+       :param max_player_id: the player we evaluate from/for
     """
-    actions = generate_actions(board, player_id)
-
     # no actions -> losing state for current player
+    actions = generate_actions(board, player_id)
     if depth == 0 or not actions:
         return evaluate(board, max_player_id), None
+
+    best_action = None
 
     if maximizing:
         value = -INF
         for action in actions:
             worker, move, build = action
-
-            prev_state = board.clone() # get the board state
-            move_worker(board, worker, move)
-            if is_win_after_move(prev_state, worker.pos, move):
-                board = prev_state
+            new_board = board.clone()
+            # find worker in new_board
+            try:
+                new_worker = next(w for w in new_board.workers if w.id == worker.id)
+            except StopIteration:
+                # Worker not found in clone? fallback to invalid action skip
+                continue
+            won = move_worker(new_board, new_worker, move)
+            if won:
                 return INF, action
-            build_block(board, worker, build)
+            build_block(new_board, new_worker, build)
 
             # call the function again, with depth decreased, and this time we minimize (coz other player's move)
-            score,_=minimax(board, depth-1, other(player_id), max_player_id, alpha, beta, maximizing = False)
-
-            board = prev_state
-
+            score, _ = minimax(new_board, depth-1, other(player_id), max_player_id, alpha, beta, False)
             if score > value:
                 value = score
                 best_action = action
@@ -53,17 +52,17 @@ def minimax(board, depth: int, player_id: str, max_player_id: str,
         value = INF
         for action in actions:
             worker, move, build = action
+            new_board = board.clone()
+            try:
+                new_worker = next(w for w in new_board.workers if w.id == worker.id)
+            except StopIteration:
+                continue
+            won = move_worker(new_board, new_worker, move)
+            if won:
+                return -INF, action  # opponent winning is bad for root?
+            build_block(new_board, new_worker, build)
 
-            prev_state = board.clone() # get the board state
-            move_worker(board, worker, move)
-            if is_win_after_move(prev_state, worker.pos, move):
-                board = prev_state
-                return INF, action
-            build_block(board, worker, build)
-
-            score,_=minimax(board, depth-1, other(player_id), max_player_id, alpha, beta, maximizing = True)
-            board = prev_state
-
+            score, _ = minimax(new_board, depth-1, other(player_id), max_player_id, alpha, beta, True)
             if score < value:
                 value = score
                 best_action = action
@@ -73,14 +72,12 @@ def minimax(board, depth: int, player_id: str, max_player_id: str,
         return value, best_action
 
 def generate_actions(board, player_id) -> List[Action]:
-    """Return all legal (worker, move, build) triples for the player."""
     actions = []
     workers = [w for w in board.workers if w.owner == player_id]
     for worker in workers:
-        for move, build in all_legal_actions(board, player_id):
-            actions.append((worker, move, build))
+        for move in legal_moves(board, worker.pos):
+            for build in legal_builds(board, move):
+                actions.append((worker, move, build))
     return actions
 
-
-def other(player_id: str) -> str:
-    return "P1" if player_id == "P2" else "P2"
+def other(player_id): return "P1" if player_id=="P2" else "P2"
