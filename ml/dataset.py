Correct normalizing + debug logging

ml/dataset.py
@@ -9,14 +9,17 @@ class SantoDataset:
         self.actions = []
         self.scores=[]
 
-    def add_sample(self, board, player_id, action, score):
+    # Normalization factor to decide how we work with our old heuristic scores
+    def add_sample(self, board, player_id, action, score, NORMALIZATION_FACTOR=1000):
         state_tensor = encode_board(board, player_id)
         action_tensor = encode_action(*action)
         self.states.append(state_tensor)
         self.actions.append(action_tensor)
-        score = min(1, max(-1, score/10))
+
+        score = np.clip(score / NORMALIZATION_FACTOR, -1.0, 1.0)
         self.scores.append(score)
         print(f"[DEBUG] state_tensor shape: {state_tensor.shape}\n action_tensor shape: {action_tensor.shape}")  # (11,5,5) + (3,5,5)
+        print(f"[DATASET'S DEBUG] Score: {score}\n")
 
     def save(self, path):
         if len(self.states) == 0: